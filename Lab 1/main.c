75cc3782eb395c1671402390ec6b7d6736832b09
LisaJacklin <lmjacklin@alaska.edu>
2023-02-11 16:48:20 -0900

    complete Code: ready for testing

--- Lab 1/main.c
+++ Lab 1/main.c
@@ -24,8 +24,8 @@ int x;
 void
 main(void)
 {
-  P7DIR = BIT0;                       //THIS SHOULD SET THE PIN TO CRYSTAL MODE
-  P7SEL |= BIT0 + BIT1;               //THIS SHOULD TELL THAT HEY! THE SPECCIL CRYSTAL IS HERE PLS USE ME
+  P7DIR = BIT0;                         //THIS SHOULD SET THE PIN TO CRYSTAL MODE
+  P7SEL |= BIT0 + BIT1;                 //THIS SHOULD TELL THAT HEY! THE SPECCIL CRYSTAL IS HERE PLS USE ME
   
   //THIS WHILE LOOP WILL CHECK TO MAKE SURE THE CRYSTAL IS ONLINE
   while (UCSCTL7 & XT1LFOFFG) {
@@ -33,52 +33,55 @@ main(void)
     }
 
   //these should drop the voltage to the lowest voltage of the core before changing the clock
-    DecrementVcore();
-    DecrementVcore();
-    DecrementVcore();
+
+  IncrementVcore();                     //this should be used as many times as needed to make sure the voltage 
+  IncrementVcore();                     //is in the correct top range of 2.4 to 3.6 V
+  IncrementVcore();                     // this should raise the max core voltage three times
 
 //Objective 1: setting up MCLK to read close to 25MHz
-   UCSCTL1 = DCORSEL_6;            //THIS SETS THE FREQUENCY RANGE
-   UCSCTL2 = 762;                  //THIS SETS THE DIVISOR FOR FREQUENCY
+   UCSCTL1 = DCORSEL_6;                 //THIS SETS THE FREQUENCY RANGE
+   UCSCTL2 = 762;                       //THIS SETS THE DIVISOR FOR FREQUENCY
 
-   UCSCTL3 |= SELREF_2;            //USING REFOCLK AS THE REFERENCED CLOCK
-   UCSCTL4 |= SELM_2 ;             //SELM 2 SETS THE ACLK SOURCE TO REFOCLK 
+   UCSCTL3 |= SELREF_2;                 //USING REFOCLK AS THE REFERENCED CLOCK
+   UCSCTL4 |= SELM_2 ;                  //SELM 2 SETS THE ACLK SOURCE TO REFOCLK 
 
-   P11SEL |= BIT0;                 // this should set the functionality to periperial
-   P11OUT |= BIT1 ;                // this should give an output to 11.1
+   P11SEL |= BIT0;                      // this should set the functionality to periperial
+   P11OUT |= BIT1 ;                     // this should give an output to 11.1
 
 //Objective 2: setting the core voltage to be the max level
 
   //ADD A BREAKPOINT BEFORE THESE INCREMENTS IN ORDER TO STEP THROUGH AND TEST THE VOLTAGE
 
-   IncrementVcore();                 //this should be used as many times as needed to make sure the voltage 
-   IncrementVcore();                 //is in the correct top range of 2.4 to 3.6 V
-
 //Objective 3(Main): measure time between button presses
 
   //TIMER SETUP FOR MAIN OBJECTIVE
-    TA0CTL = TASSEL_2;                //SETS THE BASE CLOCK TO BE SMCLK
-    //CCIE ^= BIT4;                      //ALLOWS INTERRUPTS
-    //CCIS   SHOULD THIS BE USED AS WELL?
+   P7SEL |= 0X03;                        //XT1 CLOCK PINS
+   UCSCTL6 &= ~(XT1OFF);                 //CHECKING TO SEE IF THE CLOCK IS OFF
+   UCSCTL6 |= XCAP_3;                    //LOAD CAPACITOR SPECIFYER
+
+
+   //THIS IS TO MAKE SURE NO FAULT FLAGS HAVE OCCURRED.
+   do {
+       UCSCTL7 &= ~(XT1LOFFG + DCOFFG); //CLEARS CLOCK INTERRUPT FLAGS
+       SFRIFG1 &= ~OFIFG;               //THIS SHOULD CLEAR THE FLAG 
+   } while ((SFRIFG1 & OFIFG));         //only performs if this is occuring
+
+   P11DIR |= BIT0;                      // sets the bit for the base clock frequency
+   P11SEL |= BIT0;                      //selects the correct clock for the ACLK
+
+   //sets timmer A to depend on the ACLK and to be continuous 
+   TA0CTL = TASSEL_ACLK + MC_CONTINUOUS;
+
+   P5DIR |= BIT0;
+   P5OUT &= ~BIT0;
 
   //PUSH BUTTON TO TIME REACTION SETUP
     //push button is P2.7 so BIT7 is what we are messing with
-    P2DIR &= ~BIT7;                     //sets to output direction
-    P2SEL &= ~BIT7;                     //this sets to I/O mode
+    P2DIR &= ~BIT6;                    //sets to input direction
+    P2IE |= BIT6;                      // interrupts are allowed for this button                  
+    P2REN = BIT6;                      //input with pull up resistor 
+    P2OUT = BIT6;                      //also required to enable pull up resistor.
 
-    P2IE |= BIT7;                      // interrupts are allowed for this button                  
-    P2IES |= BIT7;                      //this means the value should be 1 to enable
-    P2REN |= BIT7;                      //input with pull up resistor 
-    P2OUT |= BIT7;                      //also required to enable pull up resistor.
-
-
-  //THIS IS TO MAKE SURE NO FAULT FLAGS HAVE OCCURRED.
-    while (SFRIFG1&OFIFG)                 //TEST OSCILLATOR FAULT FLAG
-      {
-      UCSCTL7 &=~(XT2OFFG + XT1LFOFFG + XT1HFOFFG+DCOFFG);
-      //CLEAR XT2, XT1, DCO FAULT FLAGS
-      SFRIFG1 &=~OFIFG;                   //CLEAR FAULT FLAGS
-      }
 
 //THIS MUST STAY HERE AT THE END OF THE PROGRAM
  _EINT();
@@ -86,21 +89,25 @@ main(void)
 }
 
 void InterruptService_BUTTON(void) __interrupt [PORT2_VECTOR] {
-    //push button is P2.7 
-    //SET UP A WAY TO DO THE FIRST OR SECOND PUSH OF THE BUTTON...
-    x = 0;
+    //when there is an interrupt, the button constant must be changed
+    //button constant is x.
+    if (P21V == P2IV_P2IFG6) {
+        x ^= BIT0;                     //toggles x when the button is pressed
+    }
+     
+    //when the button constant is toggled to one
+    if (x == 1) {
+        TA0CTL |= TACLR;               //resets the timer; clear the timer on initial press
+        P5OUT ^= BIT0;                 //displays an output when pressed
+    }
 
-    if (P2IV == P2IV_P2IFG7){
-      x++;
-     P1OUT ^= BIT0;                   //this should toggle the LED in use
+    //if the button const is toggled to zero
+    if (x == 0) {
+        P5OUT ^= BIT0;                  //displays an output when button at zero
+        x = 0;                          //verify and set the loop again.
+    }
 
-     TA0CCR1 ^= BIT1;                     //THIS SHOULD CAPTURE...
-
-     } else {
-     P1OUT = BIT0;                    //THIS SHOULD TURN THE LED IN USE, OFF
-     TA0CCR2 ^= BIT1;                 //THIS SHOULD CAPTURE...
-     }
+}
     
-   // X = TA0CCR2- TA0CCR1; //WOULD THIS BE A POSSIBLE WAY TO GET THE CAPTURE DIFFERENCE?
 
 }  
