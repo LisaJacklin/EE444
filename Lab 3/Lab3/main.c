5eb219fe8fdc66fa7cb28d022bbd61b72c33f4a5
Lisa Jacklin <lmjacklin@alaska.edu>
2023-03-02 09:08:06 -0900

    updates

--- Lab 3/Lab3/main.c
+++ Lab 3/Lab3/main.c
@@ -19,30 +19,29 @@ OBJECTIVES:
 extern int IncrementVcore(void);
 
 int buttonpressed = 0;
+unsigned int temperature;         //converted value of temperature
 
 void main(void) {
   //in order to reach the required frequency level, the voltage must be bumped up.
   IncrementVcore();
   IncrementVcore();
-  IncrementVcore();
+  //IncrementVcore();
 
 //1. SET SMCLK AND MCLK TO BE 16 MHZ
-
     //first setting up MCLK and SMCLK to 14 MHz
     UCSCTL1 = DCORSEL_6;                                    //DOUBLE CHECK THAT THIS IS THE CORRECT SETTING RANGE
-    UCSCTL2 = 487;                                          //N = 487 SO  n+1 = 488. this is the frequency divider
+    UCSCTL2 = 427;                                          //N = 487 SO  n+1 = 488. this is the frequency divider
     UCSCTL3 = SELREF_0;                                     //AUTOMATICALLY SETS TO ZERO HERE FOR THE USE OF THE XT1CLK
     //THIS SHOULD SET BOTH SMCLK AND MCLK TO BE DEPENDENT ON THE crystal clock 
-    UCSCTL4 = SELS__XT1CLK + SELM__XT1CLK;  
-    P7SEL |= (BIT0 + BIT1);                                 //THIS MUST BE SET FOR XT1CLK EXTERNAL TO WORK PROPERLY
+    P7SEL |= (BIT0 + BIT1); 
     UCSCTL6 &=~BIT0;                                        //THIS SETS THAT THE EXTERNAL CLOCK CRYSTAL IS ON FOR USAGE.
-
+    UCSCTL4 = SELS__XT1CLK + SELM__XT1CLK;  
+                                //THIS MUST BE SET FOR XT1CLK EXTERNAL TO WORK PROPERLY
     //SETTING UP PIN TO ALLOW FOR THE FREQUENCY TO BE CHECKED.
     P11DIR |= BIT0;                                        // sets the bit for the base clock frequency
     P11SEL |= BIT0;                                        //selects the correct clock for the ACLK
    
 // 2. SETUP BUTTON SWITCH S1
-
     //SETUP BUTTON SWITCH S2 -- BOARD 3 DOES NOT HAVE A S1 BUTTON 
     //note THAT THE BUTTON SWITCH S2 IS PIN 2.7
       P2DIR &= ~BIT7;                                      //sets to input direction
@@ -52,7 +51,6 @@ void main(void) {
       P2OUT = BIT7;                                        //also required to enable pull up resistor.
 
   //SET TIMER A FOR US TO BE ABLE TO SEE THE RESULTS TAKEN FOR ANY OF THE ADC12 TIMER....?
-
     //TIMER SETUP FOR MAIN OBJECTIVE
      P7SEL |= 0X03;                                        //XT1 CLOCK PINS
      UCSCTL6 &= ~(XT1OFF);                                 //CHECKING TO SEE IF THE CLOCK IS OFF
@@ -66,14 +64,12 @@ void main(void) {
      //sets timer A to depend on the SMCLK and to be continuous 
      TA0CTL = TASSEL__SMCLK + MC__CONTINUOUS;
 
-
 //NOW, SETTING UP THE ADC12
     //noting to set the clk to 16 cycles in the sampling period.
     //ADJUST THE SAMPLING PERIOD, CLOCK IS ON, AND BASE IS SET TO SMCLK.
     ADC12CTL0 = (ADC12SHT02_H | ADC12ON_L | ADC12ENC_L | ADC12REFON_L | ADC12MSC ); 
     ADC12CTL1 = (ADC12SSEL_3 | ADC12SHS_1); 
     ADC12CTL2 = ADC12TCOFF | ADC12RES_2;                //THIS SHOULD SET THE TEMPERATURE SENSOR TO BE ONE
-
     ADC12IE = ADC12IE0;                                  //enables inTerrupts
     //note THAT 1 IS OFF FOR THE SENSOR
     ADC12MCTL0 = ADC12INCH_10 + ADC12SREF0;              //THIS IS THE TEMPERATURE DIODE
@@ -100,30 +96,37 @@ void InterruptService_BUTTON(void) __interrupt [PORT2_VECTOR] {
     //THIS WILL START THE CONVERSION AND ENABLES CONVERSION WHEN THE BUTTON IS PRESSED
     ADC12CTL0 |= ADC12ENC | ADC12SC;                     //ENABLES CONVERSION AND SAYS TO START CONVERSION
     P5OUT ^= BIT0;
+    temperature = ADC12MEM0;    //raw converted value
+
   }
 
-  //do I need to press the button twice? no...
-  else{ //if the button is 0, toggle
-    P5OUT ^= BIT0;
-    buttonpressed = 0; 
-  }
  }
 
 //THEN SETUP THE ADC12 ISR
  void InterruptSeverice_ADC12A(void) __interrupt [ADC12_VECTOR] {
  //IN THE ADC12 ISR CAPTURE CURRENT TIMER VLUE, CALCULATE AVERAGE TEMP IN C
 
- switch (__even_in_range( ADC12IV, ADC12IV_ADC12IFG0)) {
-//  case first:
 
-  //must be at the end of the interrupt.
-  //__bic_SR_register_on_exit(LPM0_bits | GIE );      //this is the ending to clear interrupts 
-  //break;
+  ADC12CTL0 = ADC12ENC_L;       //this should disable the adc12 before turning off.
+  ADC12CTL0 = ADC12ON_L;        //turns the ADC12 off
+  ADC12CTL0 = REFON_L;          //turns off the reference voltage.
 
- default: break;
+  if (ADC12IV != 0x0006) {
+    for (;;){}                  //loop here incase an error pops
+  }
+  else{
+    temperature = ADC12MEM0;    //raw converted value
+    temperature *= 4200;        //scaling factor (Vref/temp coefficient)
+    temperature += 2048;       //allow for rounding in division
 
+    //temperature /= 4096;  this should be the range of ADC12 that must be divided.
+
+    temperature -= 2780;      //subtract offset to give celsius.
+    //need to now display the value for temperature...
+
+
+  }
+
+  
  }
-
- }
-
 
